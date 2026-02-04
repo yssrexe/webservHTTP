504013aee572873cb74da79d8d4ae0d74a8f2e22
yssrexe <elyasiniyassir40@gmail.com>
2026-02-04 09:18:18 +0100

    fix parsing issues pt2

---
+++ www/html/view/test.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+import time
+
+print("Content-Type: text/html")
+print("Status: 200 OK")
+print("Set-Cookie: session=abc123; HttpOnly")
+print("\r\n\r\n")
+
+
+def add(a, b):
+    return a + b
+
+print("<html><body>")
+print("<br>")
+print("hello yassir, Welcome to webserv project")
+print("<br>")
+print("\r\n", end='')
+print(f"your age is {add(10, 12)}")
+print("</body></html>")
